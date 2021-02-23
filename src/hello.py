5c5ef368dfc405875f62a08929c640aad392ee11
Ubuntu <ubuntu@ip-172-31-56-158.us-west-2.compute.internal>
2021-02-23 00:05:17 +0000

    fixed typo

--- src/hello.py
+++ src/hello.py
@@ -5,5 +5,5 @@ import requests
 import json 
 
 r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Tel-aviv&units=metric&APPID=4265b7ed4ce93d37f19589f7324b4cfb')
-Print("Hello Iguazio")
-Print ("The weather in Tel-Aviv is:" +json.loads(json.dumps(r.json()))['main']['temp'] + " Celsius")
+print ("Hello Iguazio")
+print ("The weather in Tel-Aviv is:" +json.loads(json.dumps(r.json()))['main']['temp'] + " Celsius")
